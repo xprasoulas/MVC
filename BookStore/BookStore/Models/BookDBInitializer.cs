3a9bef55762582237a513fa4d4fffb8c924f18b9
Charis <xprasoulas@gmail.com>
2019-01-14 22:48:10 +0200

    Initiallize data to db using the "Seed" method

--- BookStore/BookStore/Models/BookDBInitializer.cs
+++ BookStore/BookStore/Models/BookDBInitializer.cs
@@ -10,7 +10,27 @@ namespace BookStore.Models
     {
         protected override void Seed(BookStoreContext context)
         {
-            base.Seed(context);
+            context.Books.Add(new Book()
+            {
+                Name = "Teach Yourself C#",
+                PublicationDate = new DateTime(2018, 6, 21),
+                Price = 45,
+                Genre = "Science"
+            });
+            context.Books.Add(new Book()
+            {
+                Name = "Programming with C#",
+                PublicationDate = new DateTime(2016, 2, 19),
+                Price = 32,
+                Genre = "IT"
+            });
+            context.Books.Add(new Book()
+            {
+                Name = "MVC in ASP.NET",
+                PublicationDate = new DateTime(2017, 3, 25),
+                Price = 87,
+                Genre = "Informatics"
+            });
         }
     }
 }
