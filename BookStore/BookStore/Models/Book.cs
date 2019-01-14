142aaebe27c8670a13c052c4f37029602d8739cb
Charis <xprasoulas@gmail.com>
2019-01-14 22:30:54 +0200

    Add/Fill Class Book

---
+++ BookStore/BookStore/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        //Τα παρακατω ονομαζονται-Data Notations
+        [Display(Name = "Publication Date")]//decoration to properties ακριβως απο πανω
+        [DataType(DataType.Date)] // αλλαζουμε την εμφανιση της ωρας
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+    }
+}
