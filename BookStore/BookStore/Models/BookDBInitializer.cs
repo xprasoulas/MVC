2a962890e6606cc1f4dedf7c32ce425fa7b158c7
Charis <xprasoulas@gmail.com>
2019-01-14 22:44:41 +0200

    Create/Fill Class BookDbInitializer

---
+++ BookStore/BookStore/Models/BookDBInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;//
+
+namespace BookStore.Models
+{
+    public class BookDBInitializer : DropCreateDatabaseAlways<BookStoreContext>
+    {
+        protected override void Seed(BookStoreContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
