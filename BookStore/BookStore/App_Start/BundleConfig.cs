710311021c0b93076df7cf3d5fe0756fc0d53698
Charis <xprasoulas@gmail.com>
2019-01-16 23:51:55 +0200

    Add to BundleConfig the new library Unobtrusive

--- BookStore/BookStore/App_Start/BundleConfig.cs
+++ BookStore/BookStore/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace BookStore
 {
@@ -12,6 +11,7 @@ namespace BookStore
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.Unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
